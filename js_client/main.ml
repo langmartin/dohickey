e6605e84f9ad0d54d53a04f6a745029e6ed072fe
Lang Martin <lang.martin@gmail.com>
2025-07-19 16:47:16 -0400

    client main doesn't work

--- js_client/main.ml
+++ js_client/main.ml
@@ -9,6 +9,21 @@ let recv_from_worker ~view e =
   let data : Jstr.t = Message.Ev.data (Ev.as_type e) in
   El.set_children view [El.p El.[txt Jstr.(v "Worker says: " + data)]]
 
+(* let query =
+  let open Js_of_ocaml in
+
+let draw_header cols =
+  let row = Document.find_el_by_id G.document (Jstr.of_string "#table_headers") in
+  let kids = List.map (fun col ->
+      El.th [El.txt' col.value]
+    )
+      cols
+  in
+  El.set_children row kids
+
+let draw_table rows =
+*)
+
 let main () =
   let h1 = El.h1 [El.txt' "Test workers"] in
   let info = El.p [ El.strong [El.txt' "Note."];
