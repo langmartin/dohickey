83e87e4c759ce6708e34e81182a235fe45aff9c7
Lang Martin <lang.martin@gmail.com>
2025-08-03 22:07:08 -0700

    convert items to jvs too

--- js_common/jv_item.ml
+++ js_common/jv_item.ml
@@ -1,3 +1,4 @@
+open Dohickey.Coda
 open Dohickey.Item
 
 let jint key jv = Jv.Int.get jv key
@@ -7,7 +8,7 @@ let jobj key jv = Jv.get jv key
 let coda_of_jv jv =
   let time = jstr "time" jv in
   let user = jstr "user" jv in
-  Dohickey.Coda.({time; user})
+  {time; user}
 
 let of_jv jv =
   let coda = jobj "coda" jv |> coda_of_jv in
@@ -17,4 +18,27 @@ let of_jv jv =
     let col = jint "col" jv in
     let text = jstr "text" jv in
     Some {coda; body = Text {row; col; text}}
+  | _ -> None
+
+let jv_of_coda coda =
+  Jv.obj [|
+    ("time", Jv.of_string coda.time);
+    ("user", Jv.of_string coda.user)
+  |]
+
+let of_item item =
+  let coda = jv_of_coda item.coda in
+  match item.body with
+  | Text i ->
+    let body = Jv.obj [|
+        ("row", Jv.of_int i.row);
+        ("col", Jv.of_int i.col);
+        ("text", Jv.of_string i.text)
+      |] in
+    Some (Jv.obj [|
+        ("coda", coda);
+        ("body", body);
+        ("type", Jv.of_string "text")
+      |])
+
   | _ -> None
