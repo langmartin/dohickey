ea08f93d80ec96fecaf756e98b940f31d6024efa
Lang Martin <lang.martin@gmail.com>
2025-08-30 15:56:27 -0400

    recv on the client

---
+++ js_client/draw.ml
@@ -0,0 +1,8 @@
+(*
+
+   The service worker receives incoming messages and turns sends them
+   as messages to us, on the client. These receivers draw the updates.
+
+*)
+
+let dims (row, col) = (row, col)
